50f7170ad844e4a8729141425ba2ca47c6ecc40a
91877 <91877@LAPTOP-BNEF5TES>
2022-11-30 18:51:16 +0530

    Test Build 2

--- src/main/java/genericLibrary/BaseClass.java
+++ src/main/java/genericLibrary/BaseClass.java
@@ -11,6 +11,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseClass {
 
 	public WebDriver driver;
@@ -21,6 +23,7 @@ public class BaseClass {
 	@BeforeMethod
 	public void openApp() throws EncryptedDocumentException, IOException
 	{
+		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.get(efd.getData("Sheet1", 0, 1));
 		driver.manage().window().maximize();
